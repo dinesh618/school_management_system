6cf76fc089ebbf2bf284ef05a1bd71d449425580
Dinesh Chauhan <dinesh@bucle.dev>
2025-09-08 19:17:58 +0530

    add native query of department

--- src/main/java/com/school/management/repository/DepartmentRepository.java
+++ src/main/java/com/school/management/repository/DepartmentRepository.java
@@ -2,9 +2,11 @@ package com.school.management.repository;
 
 import com.school.management.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface DepartmentRepository extends JpaRepository<Department,Long> {
+   @Query(value = "select  * from  department d where d.department_name = :departmentName",nativeQuery = true)
     Department findByDepartmentName(String departmentName);
-
+    @Query(value = "select  * from  department d where d.id = :id",nativeQuery = true)
     Department findByDepartmentId(Long id);
 }
